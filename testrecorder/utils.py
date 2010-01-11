a1392ce18736ca4b7e79fd11362f27b6deb29950
alerion <alerion.um@gmail.com>
2010-01-11 19:19:35 +0200

    Improve record panel render

--- testrecorder/utils.py
+++ testrecorder/utils.py
@@ -3,10 +3,10 @@ class RequestRecord(object):
     def __init__(self, request, response):
         self.url = request.path_info
         self.method = request.method
-        self.get = request.GET
-        self.post = request.POST
+        self.get = [(k, request.GET.getlist(k)) for k in request.GET]
+        self.post = [(k, request.POST.getlist(k)) for k in request.POST]
         self.code = response.status_code
-        self.redirect_url = ''
+        self.redirect_url = response.get('Location', '') 
         
 def replace_insensitive(string, target, replacement):
     """
