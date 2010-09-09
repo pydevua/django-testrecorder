dae2b1e82141a6241ee9582fdccceaf7cc2b6660
Alerion <alerion.um@gmail.com>
2010-09-09 11:47:45 +0300

    fix README and settings.py

--- testrecorder/settings.py
+++ testrecorder/settings.py
@@ -1,7 +1,20 @@
 from django.conf import settings
 
+#fixtures loaded with recorder server and used for testing
 FIXTURES = getattr(settings, 'RECORDER_FIXTURES', [])
+
+#authentication data. For example: 
+#{'username': 'test', 'password': 'test'}
+#sended to django.contrib.auth.authenticate like **AUTH 
 AUTH = getattr(settings, 'RECORDER_AUTH', None)
-IGNORE = getattr(settings, 'RECORDER_IGNORE', [])
+
+#If AUTOLOGIN - AutoLoginMiddleware try to authenticate user using AUTH option
 AUTOLOGIN = getattr(settings, 'RECORDER_AUTOLOGIN', True)
+
+#patterns for ignoring some request.  For example:
+#('^/admin.*',)
+#if pattern.match(request.path_info) it is not recorded
+IGNORE = getattr(settings, 'RECORDER_IGNORE', [])
+
+#path to save request.FILES related to settings.MEDIA_ROOT
 FILES_PATH = getattr(settings, 'RECORDER_FILES_PATH', 'test/')
