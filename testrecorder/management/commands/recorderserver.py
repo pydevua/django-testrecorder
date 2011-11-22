76f637f3c2c2b27002500e6fa1473c5b154a3a56
alerion <alerion.um@gmail.com>
2011-11-22 12:35:55 +0200

    little fix

--- testrecorder/management/commands/recorderserver.py
+++ testrecorder/management/commands/recorderserver.py
@@ -1,54 +1,55 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from optparse import make_option
-import sys
 from testrecorder.settings import FIXTURES, TEST_EMAIL_SENDING
+
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--addrport', action='store', dest='addrport',
             type='string', default='',
-            help='port number or ipaddr:port to run the server on'),          
+            help='port number or ipaddr:port to run the server on'),
     )
     help = 'Runs a test server with data from the given fixture(s) and testrecorder toolbar.'
     args = '[fixture ...]'
 
     requires_model_validation = False
-    
+
     def __init__(self, *args, **kwargs):
         self._set_middlewares()
         self._set_email_backend()
         super(Command, self).__init__(*args, **kwargs)
-    
-    def handle(self, *fixture_labels, **options):         
+
+    def handle(self, *fixture_labels, **options):
         from django.core.management import call_command
         from testrecorder.middleware import toolbar
-        
+
         if not fixture_labels:
             fixture_labels = FIXTURES
         toolbar.fixtures = fixture_labels
         call_command('testserver', *fixture_labels, **options)
-    
+
     def _set_email_backend(self):
         if TEST_EMAIL_SENDING:
             from testrecorder.utils import MailOutbox
             from django.core import mail
             mail.outbox = MailOutbox()
             settings.EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-    
+
     def _set_middlewares(self):
         middleware_name = 'testrecorder.middleware.TestRecorderMiddleware'
         autologin_middleware_name = 'testrecorder.middleware.AutoLoginMiddleware'
         auth_middleware_name = 'django.contrib.auth.middleware.AuthenticationMiddleware'
-        
+
         MWARES = list(settings.MIDDLEWARE_CLASSES)
         if not middleware_name in settings.MIDDLEWARE_CLASSES:
             MWARES.insert(0, middleware_name)
-            
+
         if not autologin_middleware_name in settings.MIDDLEWARE_CLASSES:
             if auth_middleware_name in MWARES:
                 ind = MWARES.index('django.contrib.auth.middleware.AuthenticationMiddleware')
-                MWARES.insert(ind+1, autologin_middleware_name)
+                MWARES.insert(ind + 1, autologin_middleware_name)
             else:
                 MWARES.append(autologin_middleware_name)
-        settings.MIDDLEWARE_CLASSES = MWARES                             
+
+        settings.MIDDLEWARE_CLASSES = MWARES
--- testrecorder/middleware.py
+++ testrecorder/middleware.py
@@ -1,50 +1,51 @@
 from django.conf import settings
 from django.conf.urls.defaults import include, patterns
-from testrecorder.utils import replace_insensitive
-from testrecorder.toolbar import toolbar
-import os
-import testrecorder.urls
-from django.utils.encoding import smart_unicode
 from django.contrib.auth import login, authenticate
-from testrecorder.settings import AUTH, AUTOLOGIN, TEST_FORM_VALIDATION
 from django.template import Template
+from django.utils.encoding import smart_unicode
+from testrecorder.settings import AUTH, AUTOLOGIN, TEST_FORM_VALIDATION
+from testrecorder.toolbar import toolbar
+from testrecorder.utils import replace_insensitive
+import testrecorder.urls
+
 
 _HTML_TYPES = ('text/html', 'application/xhtml+xml')
 _STATUS_CODES = (200, 302)
 
+
 class TestRecorderMiddleware(object):
-    
+
     def __init__(self):
         self.override_url = True
         self.original_urlconf = settings.ROOT_URLCONF
-        self.original_pattern = patterns('', ('', include(self.original_urlconf)),)        
-    
+        self.original_pattern = patterns('', ('', include(self.original_urlconf)),)
+
     def process_request(self, request):
         if self.override_url:
             testrecorder.urls.urlpatterns += self.original_pattern
             self.override_url = False
-            #maybe do it safe-threading for sites with dynamic TOOR_URLCONF
+            #maybe do it safe-threading for sites with dynamic ROOT_URLCONF
             settings.ROOT_URLCONF = 'testrecorder.urls'
-        
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         if TEST_FORM_VALIDATION:
             #a little bit of magic
             original_render = Template.render
-            
+
             def new_render(obj, context):
                 if not hasattr(request, '_djtr_context'):
                     request._djtr_context = context
                 return original_render(obj, context)
-            
+
             Template.render = new_render
-        
+
     def process_response(self, request, response):
-        if self._validate_request(request, response):        
+        if self._validate_request(request, response):
             toolbar.process_response(request, response)
-            if response.status_code == 200 and response['Content-Type'].split(';')[0] in _HTML_TYPES:            
+            if response.status_code == 200 and response['Content-Type'].split(';')[0] in _HTML_TYPES:
                 response.content = replace_insensitive(smart_unicode(response.content), u'</body>', smart_unicode(toolbar.render() + u'</body>'))
         return response
-    
+
     def _validate_request(self, request, response):
         if response.status_code == 304:
             return False
@@ -53,10 +54,11 @@ class TestRecorderMiddleware(object):
         if len(settings.MEDIA_URL) and request.path.startswith(settings.MEDIA_URL):
             return False
         return True
-    
+
+
 class AutoLoginMiddleware(object):
     logged = False
-    
+
     def process_request(self, request):
         if not request.user.is_authenticated() and AUTH \
             and AUTOLOGIN and not self.logged:
